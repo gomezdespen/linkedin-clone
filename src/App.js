0b50e6fbb0a8c2a0d037d7cfe18c11e89f103fb9
daniel gomez <gomezdespen@gmail.com>
2021-01-02 02:42:11 -0500

    Logiin/user

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import React from 'react';
+import { useSelector } from "react-redux";
 import './App.css';
+import { selectUser } from "./features/userSlice";
 import Header from './Header';
 import Sidebar from './Sidebar';
 import Feed from './Feed';
+import Login from './Login';
 
 
 
 
 function App() {
+  const user = useSelector(selectUser);
+
   return (
     <div className="app">
       <Header />
 
-        {/* App Body */}
-        <div className="app__body">
-          <Sidebar />
-          <Feed />
-          {/* Widgets */}
-        </div>
+      {!user ? (
+        <Login />
+      ) : (
+          <div className="app__body">
+
+            <Sidebar />
+            <Feed />
+            {/* Widgets */}
+          </div>
+      )}        
     </div>
   );
 }
